bf124757baa7f657cd82253176f88085e0c45b70
peto-tn <guitarspirits3@gmail.com>
2016-03-17 08:12:00 +0000

    PHP版投稿リンク設置

--- webapp/php/templates/index.php
+++ webapp/php/templates/index.php
@@ -1,7 +1,7 @@
-<h2>タイムライン</h2>
+<a href="/upload_image"><h2>投稿する</h2></a>
 <div class="row panel panel-primary" id="timeline">
   <dl>
-    <dt>name</dt><dd id="prof-name"><?php h($user['name']) ?></dd>
+    <dt>name</dt><dd id="prof-name"><a href="/user/<?php h($user['id']) ?>"><?php h($user['name']) ?></a></dd>
     <dt>email</dt><dd id="prof-email"><?php h($user['email']) ?></dd>
     <dt>following</dt><dd id="prof-following"><a href="/following"><?php h(count($following)) ?></a></dd>
     <dt>followers</dt><dd id="prof-followers"><a href="/followers"><?php h(count($followers)) ?></a></dd>
